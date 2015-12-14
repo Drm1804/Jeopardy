502f00a527494bea99f28534c3e03a35420c5c50
Drm-a <drm-a@yandex.ru>
2015-12-14 21:25:39 +0500

    Сделал фабрику по генерации контента

--- src/app/question/question.js
+++ src/app/question/question.js
@@ -7,4 +7,51 @@ angular.module('jeopardy')
         url: '/question/:path',
         templateUrl: 'app/question/question.html'
       })
+  })
+  .controller('QuestionController', function($scope, hotkeys, $state, QuestionContentFactory){
+    $scope.steps = {
+      step1: {
+        show: true,
+        content: ''
+      },
+      step2: {
+        show: false,
+        content: ''
+      },
+      step3: {
+        show: false,
+        content: ''
+      },
+      step4: {
+        show: false,
+        content: ''
+      }
+    };
+    $scope.path = '';
+
+    $scope.getPath = function(){
+      $scope.path = $state.params.path;
+    };
+
+    $scope.getPath();
+
+    $scope.generateContent = function(){
+      for(var item in $scope.steps){
+        QuestionContentFactory.generateContent();
+      }
+    };
+    
+    $scope.generateContent();
+  })
+  .factory('QuestionContentFactory', function(){
+    var _this = this;
+
+    return{
+      generateContent: function(path, step, typeContent){
+        console.log('1')
+      }
+    }
+
   });
+
+
